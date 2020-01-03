8a898b546e1c303357edcfdad8bd6a358b772d4b
Robin <robinkimzey@gmail.com>
2020-01-03 11:05:20 -0800

    add socket.io functionality to app.js file

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const app = require('express')()
 const http = require('http').Server(app)
+const io = require('socket.io')(http)
 
 app.get('/', function(req, res) {
   res.sendfile('index.html')
+});
+
+//Whenever someone connects this gets executed
+io.on('connection', function(socket) {
+  console.log('A user connected')
+
+  //Whenever someome disconnects this piece of code is executed
+  socket.on('disconnect', function() {
+    console.log('A user disconnected')
+  });
 });
 
 http.listen(3000, function() {
