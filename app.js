276f2328b981bc211fb913f1f4fd674ad586d088
Robin <robinkimzey@gmail.com>
2020-01-04 19:45:25 -0800

    change files to use a namespace

--- app.js
+++ app.js
@@ -6,18 +6,23 @@ app.get('/', function(req, res) {
   res.sendfile('index.html')
 });
 
-let clients = 0;
+let nsp = io.of('/my-namespace')
+nsp.on('connection', function(socket) {
+  console.log('someone connected')
+  nsp.emit('hi', 'Hello everyone!')
+})
+//let clients = 0;
 //Whenever someone connects this gets executed
-io.on('connection', function(socket) {
-  clients++
-  socket.emit('newclientconnect', {description: 'Hey, welcome!'});
-  socket.broadcast.emit('newclientconnect', {description: clients + ' clients connected!'})
-  //Whenever someome disconnects this piece of code is executed
-  socket.on('disconnect', function() {
-    clients--
-    socket.broadcast.emit('broadcast', {description: clients + ' clients connected!'});
-  });
-});
+//io.on('connection', function (socket) {
+//  clients++
+//  socket.emit('newclientconnect', { description: 'Hey, welcome!' });
+//  socket.broadcast.emit('newclientconnect', { description: clients + ' clients connected!' })
+//  //Whenever someome disconnects this piece of code is executed
+//  socket.on('disconnect', function () {
+//    clients--
+//    socket.broadcast.emit('broadcast', { description: clients + ' clients connected!' });
+//  });
+//});
 
 http.listen(3000, function() {
   console.log('listening on *:3000')
